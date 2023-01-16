b7b9609b4f89cc7e8cda27afaf871620fb885b36
Ping <roselinepli@gmail.com>
2023-01-15 22:24:46 -0500

    add hw1

---
+++ homework/hw1.py
@@ -0,0 +1,97 @@
+
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    >>> a_plus_abs_b(-1, 4)
+    3
+    >>> a_plus_abs_b(-1, -4)
+    3
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a, b)
+
+def two_of_three(i, j, k):
+    """Return m*m + n*n, where m and n are the two smallest members of the
+    positive numbers i, j, and k.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    """
+    # return min(i, j) * min(i, j) + min(j, k) * min(j, k)
+    return min(i**2 + j**2, i**2 + k**2, j**2 + k**2)
+
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    # lst = []
+    # for i in range(n):
+    #     if n % i == 0:
+    #         lst.append(i)
+    #     return max(lst)
+    factor = n - 1
+    while factor > 0:
+        if n % factor == 0:
+            return factor
+        factor -= 1
+
+def hailstone(n):
+    """Print the hailstone sequence starting at n and return its
+    length.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    length = 1
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        length += 1
+    print(n)
+    return length
+
+def halistone(n):
+    print(n)
+    if n == 1:
+        return 1
+    elif n % 2 == 0:
+        return 1 + halistone(n // 2)
+    else:
+        return 1 + halistone(n * 3 + 1)
