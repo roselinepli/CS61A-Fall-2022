b06baa1df916c26e4ac6edb7c95275422acb8b4a
Ping <roselinepli@gmail.com>
2023-01-25 09:42:46 -0500

    add lab6

---
+++ lab/lab6.py
@@ -0,0 +1,90 @@
+def insert_items(lst, entry, elem):
+    """Inserts elem into lst after each occurrence of entry and then returns lst.
+
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> test_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> double_lst = [1, 2, 1, 2, 3, 3]
+    >>> double_lst = insert_items(double_lst, 3, 4)
+    >>> double_lst
+    [1, 2, 1, 2, 3, 4, 3, 4]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    idx = 0
+    while idx < len(lst):
+        if lst[idx] == entry:
+            lst.insert(idx+1, elem)
+            idx += 2
+        else:
+            idx += 1
+    return lst
+
+def count_occurrences(t, n, x):
+    """Return the number of times that x appears in the first n elements of iterator t.
+
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s, 10, 9)
+    3
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s2, 3, 10)
+    2
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> count_occurrences(s, 1, 3)
+    1
+    >>> next(s)
+    2
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> count_occurrences(s2, 6, 6)
+    2
+    """
+    cnt = 0
+    for i in range(n):
+        num = next(t)
+        if num == x:
+            cnt += 1
+    return cnt
+
+def repeated(t, k):
+    """Return the first value in iterator T that appears k times in a row.
+    Iterate through the items such that if the same iterator is passed into
+    the function twice, it continues in the second call the point it left
+    off in the first.
+
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s, 2)
+    9
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s2, 3)
+    8
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> repeated(s, 3)
+    2
+    >>> repeated(s, 3)
+    5
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> repeated(s2, 3)
+    2
+    """
+    assert k > 1
+    prev = -1
+    cnt = 0
+    for x in t:
+        if x == prev:
+            cnt += 1
+            if cnt == k:
+                return x
+        else:
+            prev = x
+            cnt = 1
+    return -1
