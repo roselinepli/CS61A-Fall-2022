4427d21b73eb72eabc3ca10db2b390fc6c31cc45
Ping <roselinepli@gmail.com>
2023-03-07 09:39:54 -0500

    problem 6 solved with tests

--- projects/hog/hog.py
+++ projects/hog/hog.py
@@ -184,7 +184,9 @@ def always_roll(n):
     """
     assert n >= 0 and n <= 10
     # BEGIN PROBLEM 6
-    "*** YOUR CODE HERE ***"
+    def always_roll_n(score, opponent_score):
+        return n
+    return always_roll_n
     # END PROBLEM 6
 
 
