adf2e05d85388e3c889ba0d1ca12357c8565a0fe
Ping <roselinepli@gmail.com>
2023-03-14 10:08:38 -0400

    unlock truck tests

--- lab/lab8/tests/wwpd-truck.py
+++ lab/lab8/tests/wwpd-truck.py
@@ -10,40 +10,30 @@ test = {
           >>> deneros_car = Car('Tesla', 'Model S')
           >>> deneros_truck = MonsterTruck('Monster', 'Batmobile')
           >>> deneros_car.size # Type Error if an error occurs and Nothing if nothing is displayed
-          8b1b16415e2c0ff19bfb5c4e54f6f878
-          # locked
+          'Tiny'
           >>> deneros_car.drive() # Type Error if an error occurs and Nothing if nothing is displayed
-          6782dfbb8a7616b3f504afa7bdbc4efe
-          # locked
+          'Tesla Model S goes vroom!'
           >>> deneros_truck.size # Type Error if an error occurs and Nothing if nothing is displayed
-          c5ace7df7c7b66fd8733af007dd50564
-          # locked
+          'Monster'
           >>> deneros_truck.drive() # Type Error if an error occurs and Nothing if nothing is displayed
-          543d3c4044c7b885e6bc773187315cb9
-          2e96bc0878a8a5a4b77deec4ef4b3d09
-          # locked
+          Vroom! This Monster Truck is huge!
+          'Monster Batmobile goes vroom!'
           >>> MonsterTruck.drive(deneros_truck) # Type Error if an error occurs and Nothing if nothing is displayed
-          543d3c4044c7b885e6bc773187315cb9
-          2e96bc0878a8a5a4b77deec4ef4b3d09
-          # locked
+          Vroom! This Monster Truck is huge!
+          'Monster Batmobile goes vroom!'
           >>> Car.drive(deneros_truck) # Type Error if an error occurs and Nothing if nothing is displayed
-          2e96bc0878a8a5a4b77deec4ef4b3d09
-          # locked
+          'Monster Batmobile goes vroom!'
           >>> deneros_truck.gas # Type Error if an error occurs and Nothing if nothing is displayed
-          7c2ddff07764c87227f4781f812caaa6
-          # locked
+          0
           >>> MonsterTruck.rev(deneros_truck) # Type Error if an error occurs and Nothing if nothing is displayed
-          543d3c4044c7b885e6bc773187315cb9
-          # locked
+          Vroom! This Monster Truck is huge!
           >>> MonsterTruck.rev(deneros_car) # Type Error if an error occurs and Nothing if nothing is displayed
-          543d3c4044c7b885e6bc773187315cb9
-          # locked
+          Vroom! This Monster Truck is huge!
           >>> Car.rev(deneros_truck) # Type Error if an error occurs and Nothing if nothing is displayed
-          8a2fd4e4c3c6dcce2dc631af62ee217a
-          # locked
+          Error
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
