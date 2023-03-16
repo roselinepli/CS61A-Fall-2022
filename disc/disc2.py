71e0be8ff15f61493e60d0ce943972e9c23c4fc3
Ping <roselinepli@gmail.com>
2023-03-16 10:55:55 -0400

    disc2 finished

--- disc/disc2.py
+++ disc/disc2.py
@@ -16,3 +16,89 @@ def make_keeper(n):
                 print(i)
             i += 1
     return do_keep
+
+def curry(func):
+    """
+    Returns a Curried version of a two_argument function FUNC.
+    >>> from operator import add, mul, mod
+    >>> curried_add = curry(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    >>> curried_mul = curry(mul)
+    >>> mul_5 = curried_mul(5)
+    >>> mul_5(42)
+    210
+    >>> curry(mod)(123)(10)
+    3
+    """
+    def first(arg1):
+        def second(arg2):
+            return func(arg1, arg2)
+        return second
+    return first
+
+def f1():
+    """
+    >>> f1()
+    3
+    """
+    return 3
+
+def f2():
+    """
+    >>> f2()()
+    3
+    """
+    return lambda: 3
+
+def f3():
+    """
+    >>> f3()(3)
+    3
+    """
+    return lambda x: x
+
+def f4():
+    """
+    >>> f4()()(3)()
+    3
+    """
+    return lambda: lambda x: lambda: x
+
+
+def lambda_curry2(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add, mul, mod
+    >>> curried_add = lambda_curry2(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    >>> curried_mul = lambda_curry2(mul)
+    >>> mul_5 = curried_mul(5)
+    >>> mul_5(42)
+    210
+    >>> lambda_curry2(mod)(123)(10)
+    3
+    """
+    return lambda arg1: lambda arg2: func(arg1, arg2)
+
+def match_k_alt(k):
+    """ Return a function that checks if digits k apart match
+
+    >>> match_k_alt(2)(1010)
+    True
+    >>> match_k_alt(2)(2010)
+    False
+    >>> match_k_alt(1)(1010)
+    False
+    >>> match_k_alt(1)(1)
+    True
+    >>> match_k_alt(1)(2111111111111111)
+    False
+    >>> match_k_alt(3)(123123)
+    True
+    >>> match_k_alt(2)(123123)
+    False
+    """
