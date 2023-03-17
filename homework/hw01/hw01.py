7a95f18d3a782c75922a84076c0a58b1459213fd
Ping <roselinepli@gmail.com>
2023-03-17 10:09:23 -0400

    upload files

--- homework/hw01/hw01.py
+++ homework/hw01/hw01.py
@@ -101,3 +101,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    length = 1
+    while n != 1:
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        length += 1
+    print(n)
+    return length
