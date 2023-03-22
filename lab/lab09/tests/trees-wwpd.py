49a67ab3a1b96cdb29b011306d32d961d25d2d9f
Ping <roselinepli@gmail.com>
2023-03-22 13:16:35 -0400

    upload lab05 files

--- lab/lab09/tests/trees-wwpd.py
+++ lab/lab09/tests/trees-wwpd.py
@@ -7,18 +7,14 @@ test = {
         {
           'code': r"""
           >>> t = Tree(1, Tree(2)) # Enter Function if you believe the answer is <function ...>, Error if it errors, and Nothing if nothing is displayed.
-          feef61c63dd96e13f9fae6fd28442b2b
-          # locked
+          Error
           >>> t = Tree(1, [Tree(2)])
           >>> t.label
-          7cd20da6435c318b417f99ab831ac85e
-          # locked
+          1
           >>> t.branches[0]
-          6e3e150dcaf43b1aee92209a3f22f19f
-          # locked
+          Tree(2)
           >>> t.branches[0].label
-          32cd207d18df99546ca7a56bc36ed715
-          # locked
+          2
           >>> t.label = t.branches[0].label
           >>> t
           f331eaca6e463d6c1142a1cd9f252566
