3de180e3af3263ade2ba00503a0165966d99a014
Ping <roselinepli@gmail.com>
2023-04-04 22:11:00 -0400

    disc9

--- disc/disc9.py
+++ disc/disc9.py
@@ -46,3 +46,86 @@ def convert_link(link):
     if type(link.first) == Link:
         return [convert_link(link.first)] + convert_link(link.rest)
     return [link.first] + convert_link(link.rest)
+
+
+def duplicate_link(link, val):
+    """Mutates 'link' such that if there is a linked list node that has a first equal to
+    value, that node will be duplcated. Note that you should be mutating the
+    original link list.
+
+    >>> x = Link(5, Link(4, Link(3)))
+    >>> duplicate_link(x, 5)
+    >>> x
+    Link(5, Link(5, Link(4, Link(3))))
+    >>> y = Link(2, Link(4, Link(6, Link(8))))
+    >>> duplicate_link(y, 10)
+    >>> y
+    Link(2, Link(4, Link(6, Link(8))))
+    >>> z = Link(1, Link(2, (Link(2, Link(3)))))
+    >>> duplicate_link(z, 2) #ensures that back to back links with val are both duplicated
+    >>> z
+    Link(1, Link(2, Link(2, Link(2, Link(2, Link(3))))))
+    """
+    if link is Link.empty:
+        return
+    elif link.first == val:
+        remaining = link.rest
+        link.rest = Link(val, remaining)
+        duplicate_link(remaining, val)
+    else:
+        duplicate_link(link.rest, val)
+
+
+def multiply_links(lst_of_links):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    product = 1
+    for lnk in lst_of_links:
+        if lnk is Link.empty:
+            return Link.empty
+        product *= lnk.first
+    lst_of_links_rests = [lnk.rest for lnk in lst_of_links]
+    return Link(product, multiply_links(lst_of_links_rests))
+
+
+def flip_two(s):
+    """
+    >>> one_link = Link(1)
+    >>> flip_two(one_link)
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
+    if s is Link.empty or s.rest is Link.empty:
+        return
+    s.first, s.rest.first = s.rest.first, s.first
+    flip_two(s.rest.rest)
+
+
+def find_paths(t, entry):
+    """
+    >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
+    >>> find_paths(tree_ex, 5)
+    [[2, 7, 6, 5], [2, 1, 5]]
+    >>> find_paths(tree_ex, 12)
+    []
+    """
+    paths = []
+    if t.label == entry:
+        paths.append([t.label])
+    for b in t.branches:
+        for path in find_paths(b, entry):
+            paths.append([t.label] + path)
+    return paths
