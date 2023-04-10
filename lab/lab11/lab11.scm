e84f700b8f50f9ccdda2e59e60d0910e8cceff37
Ping <roselinepli@gmail.com>
2023-04-10 16:13:25 -0400

    lab11 - 1

--- lab/lab11/lab11.scm
+++ lab/lab11/lab11.scm
@@ -1,4 +1,5 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond ((< num1 num2) '-1)
+((= num1 num2) '0) ((> num1 num2) '1)))
 
 (define (make-adder num) 'YOUR-CODE-HERE)
 
