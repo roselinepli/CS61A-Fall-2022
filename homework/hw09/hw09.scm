4feb747021b95e1aa05af854c6f22f6a526bbbff
Ping <roselinepli@gmail.com>
2023-04-14 10:51:17 -0400

    hw09 try

--- homework/hw09/hw09.scm
+++ homework/hw09/hw09.scm
@@ -1,7 +1,18 @@
-(define (ascending? asc-lst) 'YOUR-CODE-HERE)
+(define (ascending? asc-lst)
+    (cond ((null? asc-lst) #t)
+          ((null? (cdr asc-lst)) #t)
+           ((<= (car asc-lst) (car (cdr asc-lst))) (ascending? (cdr asc-lst)))
+           (else #f)))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+    (cond((null? s) '())
+          ((pred (car s)) (cons (car s) (my-filter pred (cdr s))))
+          (else (my-filter pred (cdr s)))))
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2)
+    (cond ((null? lst1) lst2)
+           ((null? lst2) lst1)
+           (else (cons (car lst1) (interleave lst2 (cdr lst1))))))
 
-(define (no-repeats lst) 'YOUR-CODE-HERE)
+(define (no-repeats lst)
+    (cond ((define (lambda x: my-filter)))))
