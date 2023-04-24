76c9d5ea1ae4b85e8910732c47858008312e73d6
Ping <roselinepli@gmail.com>
2023-04-24 10:03:59 -0400

    lab12 1-2

--- lab/lab12/lab12.scm
+++ lab/lab12/lab12.scm
@@ -1,6 +1,6 @@
 (define (substitute s old new)
-      (cond((null? s) '())
-           (pair? (car s) (cons(substitute car(s) old new) (substitute cdr(s) old new)))
+      (cond((null? s) nil)
+           ((pair? (car s)) (cons(substitute (car s) old new) (substitute (cdr s) old new)))
            ((equal? (car s) old) (cons new (substitute (cdr s) old new)))
            (else (cons (car s) (substitute(cdr s) old new)))))
 
